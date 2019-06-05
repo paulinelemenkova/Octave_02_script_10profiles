d4434112f9aea03dbe3d47c506802f8df4c61f4e
Pauline Lemenkova <pauline.lemenkova@gmail.com>
2019-06-05 11:16:16 +0800

    Add files via upload

---
+++ Octave_install_packages.m
@@ -0,0 +1,14 @@
+# display list of installed packages
+pkg list
+# installing packages
+# install statistics package
+pkg install -forge io % dependency for statistics
+pkg install -forge statistics
+# install econometrics package
+pkg install -forge struct % dependency for optim
+pkg install -forge optim % dependency for econometrics
+pkg install -forge econometrics
+# install multivariate normal distribution clustering and utility functions
+pkg install -forge mvn
+# install image processing package
+pkg install -forge image
